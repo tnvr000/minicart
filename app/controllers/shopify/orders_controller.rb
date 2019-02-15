986f217ce0da3351b49a7fce9eb2039f2119f555
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2019-02-15 19:18:38 +0530

    added show pages for order and product, add feature to add and update metadata to an order

--- app/controllers/shopify/orders_controller.rb
+++ app/controllers/shopify/orders_controller.rb
@@ -1,20 +1,54 @@
 class Shopify::OrdersController < Shopify::MainController
-	# before_action :authorize
+	before_action :shopify_session
 	def index
-		@shop = Shop.find_by_id(params[:shop_id])
-		shopify_session = get_shopify_session
 		@orders = ShopifyAPI::Order.find(:all)
 	end
 
 	def show
-		@shop = Shop.find_by_id(params[:shop_id])
-		shopify_session = get_shopify_session
 		@order = ShopifyAPI::Order.find(params[:id].to_i)
 	end
 
 	def edit
-		@shop = Shop.find_by_id(params[:shop_id])
-		shopify_session = get_shopify_session
 		@order = ShopifyAPI::Order.find(params[:id].to_i)
+		@metafields = metafields
+		# binding.pry
 	end
+
+	def update
+		@order = ShopifyAPI::Order.find(params[:id].to_i)
+		@metafields = metafields params['metafields']
+		@metafields.each do |metafield|
+			@order.add_metafield(metafield)
+		end
+		redirect_to shopify_shop_order_url(@shop, @order)
+	rescue SocketError => e
+		retry
+	end
+
+	private
+	def metafields value={}
+		metafields = @order.metafields
+		keys = %w(length width height)
+		dim_metafields = []
+		parameters = {
+			value_type: 'integer',
+			namespace: 'dimension',
+			owner_id: "#{@order.id}",
+			owner_resource: 'order'
+		}
+		keys.each do |key|
+			metafield = metafields.select{|metafield| metafield.key == key}.first
+			(metafield.value = value[key] || metafield.value) and dim_metafields << metafield and next unless metafield.nil?
+			parameters[:key] = key
+			parameters[:value] = value[key] || '0'
+			dim_metafields << ShopifyAPI::Metafield.new(parameters)
+		end
+		return dim_metafields
+	end
+
+	def shopify_session
+		@shop = Shop.find_by_id params[:shop_id]
+		get_shopify_session
+	end
+
 end
--- config/initializers/shopify.rb
+++ config/initializers/shopify.rb
@@ -1,6 +1,6 @@
 API_KEY = "89b981c145ce0ca3f7cb17815bc131cc"
 API_SECRET_KEY = "621337a9aa7e28b910d660858205ba98"
 API_SECRET = "621337a9aa7e28b910d660858205ba98"
-APP_URL = '2af212d8.ngrok.io'
+APP_URL = '258500b0.ngrok.io'
 SCOPES = ['read_orders', 'write_orders', 'read_products', 'write_products', 'read_shipping', 'write_shipping', 'read_fulfillments', 'write_fulfillments']
 NONCE = 'scret'
